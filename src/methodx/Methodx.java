a42108e96e43bf9b13c3e9ee7e802652ddf35d95
stnvtrani <sitinovitriani@gmail.com>
2025-04-28 04:58:39 +0700

    Tambahan Volume Bola

--- src/methodx/Methodx.java
+++ src/methodx/Methodx.java
@@ -4,6 +4,8 @@
  */
 package methodx;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sitin
@@ -14,15 +16,29 @@ public class Methodx {
         double luas= Math.PI * Math.pow(jari2, 2);
         return luas;
     }
-
+ public double VolumeBola(double diameter, double jariJari){
+     double volume = (4.0 / 3) * Math.PI * Math.pow(jariJari, 3);
+        return volume;
+    }
+     
+ 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner input = new Scanner(System.in);
         Methodx mt  = new Methodx();
         System.out.println(mt.luaslingkaran(20));
-        System.exit(0);
+        
+        System.out.print("Masukkan Diameter Bola : ");
+        double diameter = input.nextInt();
+        System.out.print("Masukkan Jari jari Bola : ");
+        double jariJari = input.nextInt();
+        double volume = (mt.VolumeBola(3, 3));
+        System.out.println("Volume bola adalah: " + volume);
+
+        mt.luaslingkaran(20);
+        mt.VolumeBola(diameter, jariJari);
     }
-    
 }
